b744c268c5e71ba66c0b3eb517c6caad31aad821
Sharat Chandra MS <mssharatchandra@gmail.com>
2022-02-10 14:58:00 +0530

    first push of the project

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>Galarie de Leoshrey </h1>
+      <h2>Welcome to the art gallery of Leoshrey </h2>
+      <p>The curated gallery of Leoshrey filled with timeless interpretations and staggering perspectives</p>
+    </div>
+  )
+}
+
+export default Title;
